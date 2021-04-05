4cae2c53bf9bf315ee3f28b35619151a64419b0d
KiKiKi_KiKi <chaika.design@gmail.com>
2021-04-06 01:24:30 +0900

    :horse: Implement API of get books data from google books api.

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -3,7 +3,11 @@ import { logger } from 'firebase-functions';
 // => This module is declared with using 'export =', and can only be used with a default import when using the 'esModuleInterop' flag.
 // cf. https://qiita.com/karak/items/29ff148788f5abb15331
 import express = require('express');
-import errorHandler, { notFoundException } from './errorExceptions';
+import fetch from 'node-fetch';
+import errorHandler, {
+  notFoundException,
+  badRequestException,
+} from './errorExceptions';
 
 const app: express.Express = express();
 // eslint-disable-next-line new-cap
@@ -58,6 +62,39 @@ router.get('/users/:userId', (req: express.Request, res: express.Response) => {
 
   res.send(targetUser);
 });
+
+interface BookItem {
+  [key: string]: any;
+}
+
+interface BooksData {
+  kind: string;
+  totalItems: number;
+  items?: BookItem[];
+}
+
+const getDataFromApi = async (keyword: string): Promise<BooksData> => {
+  // https://developers.google.com/books/docs/v1/using
+  const requestUrl =
+    'https://www.googleapis.com/books/v1/volumes?country=JP&q=intitle:';
+
+  try {
+    const result = await fetch(`${requestUrl}${keyword}`);
+
+    return result.json();
+  } catch (error) {
+    throw badRequestException(error.message);
+  }
+};
+
+router.get(
+  '/books/:keyword',
+  async (req: express.Request, res: express.Response) => {
+    const keywors: string = req.params?.keyword;
+    const response = await getDataFromApi(keywors);
+    res.send(response);
+  },
+);
 
 // Routing
 app.use('/', router);
