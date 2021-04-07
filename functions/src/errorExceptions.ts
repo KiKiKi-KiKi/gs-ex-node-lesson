6b38c0008e6833b76b0f6892695352c0ff2ce970
KiKiKi_KiKi <chaika.design@gmail.com>
2021-04-08 00:36:39 +0900

    :space_invader: Add suctom data to error exception.

--- functions/src/errorExceptions.ts
+++ functions/src/errorExceptions.ts
@@ -1,41 +1,55 @@
 import { Request, Response, NextFunction } from 'express';
 
-class HttpException extends Error {
+type errorData = {
+  [key: string]: any;
+};
+
+export class HttpException extends Error {
   statusCode?: number;
   message: string;
-  constructor(statusCode: number, message: string) {
+  data: errorData;
+
+  constructor(statusCode: number, message: string, data?: errorData) {
     super(message);
     this.statusCode = statusCode || 500;
     this.message = message;
+    this.data = data ? { ...data } : {};
   }
 }
 
 export const badRequestException = (
   message = '400 Bad Request',
+  data?: errorData,
 ): HttpException => {
-  return new HttpException(400, message);
+  return new HttpException(400, message, data);
 };
 
 export const unauthorizedException = (
   message = '401 Unauthorized',
+  data?: errorData,
 ): HttpException => {
-  return new HttpException(401, message);
+  return new HttpException(401, message, data);
 };
 
 export const forbiddenException = (
   message = '403 Forbidden',
+  data?: errorData,
 ): HttpException => {
-  return new HttpException(403, message);
+  return new HttpException(403, message, data);
 };
 
-export const notFoundException = (message = '404 Not Found'): HttpException => {
-  return new HttpException(404, message);
+export const notFoundException = (
+  message = '404 Not Found',
+  data?: errorData,
+): HttpException => {
+  return new HttpException(404, message, data);
 };
 
 export const internalServerErrorException = (
   message = '500 Internal Server Error',
+  data?: errorData,
 ): HttpException => {
-  return new HttpException(500, message);
+  return new HttpException(500, message, data);
 };
 
 export default function errorHandler(
@@ -46,9 +60,9 @@ export default function errorHandler(
 ): void {
   res.status(err.statusCode || 500);
   res.send({
+    message: err.message,
     error: {
-      ...err,
-      message: err.message,
+      ...err.data,
     },
   });
 }
