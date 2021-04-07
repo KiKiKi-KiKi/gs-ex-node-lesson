c2495b75dd6d9baa378b2a7a2c3066b901fbc517
KiKiKi_KiKi <chaika.design@gmail.com>
2021-04-07 20:29:17 +0900

    Implement get all todos from firestore.

--- functions/src/Todos/todos.controller.ts
+++ functions/src/Todos/todos.controller.ts
@@ -1,12 +1,39 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+
+import { admin } from '../model/firebase';
+const db = admin.firestore();
+const COLLECTION = 'todos';
 
 // eslint-disable-next-line new-cap
 const router = Router();
 
-router.get('/', cors(), async (req: Request, res: Response) => {
-  res.send('Todos GET endpoint');
-});
+router.get(
+  '/',
+  cors(),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const todoSnapshot: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData> = await db
+        .collection(COLLECTION)
+        .orderBy('isDone', 'asc')
+        .orderBy('dueDate', 'asc')
+        .get();
+
+      const todos = todoSnapshot.docs.map(
+        (item: FirebaseFirestore.DocumentData) => {
+          return {
+            id: item.id,
+            data: item.data(),
+          };
+        },
+      );
+
+      res.send(todos);
+    } catch (err) {
+      next(err);
+    }
+  },
+);
 
 router.post('/', cors(), async (req: Request, res: Response) => {
   res.send('Todos POST endpoint');
